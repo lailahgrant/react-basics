9a61e38af10f2e40c1a3422336155d1a4781fadf
lailahgrant <lailahgrant19@gmail.com>
2020-05-20 15:19:14 +0300

    creating state to handle dynamic data

--- App.js
+++ App.js
@@ -4,6 +4,31 @@ import Table  from './Table'
 
 class App extends Component {
 
+  state = {
+    characters: [
+      {
+        name:'Lailah',
+        job: 'Cloud developer'
+      },
+      {
+        name: 'Kato',
+        job: 'Doctor'
+      },
+      {
+        name: 'Waswa',
+        job: 'Cycber spcialist'
+      },
+      {
+        name: 'Bushra',
+        job: 'Intern'
+      },
+      {
+        name: 'uliana',
+        job: 'Front-end developer'
+      }
+    ]
+  }
+
   render() {
    
     return(
--- Table.js
+++ Table.js
@@ -1,39 +1,34 @@
 import React, { Component } from 'react'
 
-class Table extends Component {
-  render(){
-    return(
-      <table>
-
-      <thead>
+const TableHeader = () => {
+  return (
+    <thead>
         <tr>
           <th>Name</th>
           <th>Job</th>
         </tr>
       </thead>
+  )
+}
 
-      <tbody>
+const TableBody = () => {
+  return (
+    <tbody>
       <tr>
-      <td>Lailah </td>
-      <td>Cloud developer</td>
-      </tr>
-      <tr>
-      <td>Kato </td>
-      <td>Doctor</td>
-      </tr>
-      <tr>
-      <td>Waswa </td>
-      <td>Cycber spcialist</td>
-      </tr>
-      <tr>
-      <td>Bushra </td>
-      <td>Intern</td>
-      </tr>
-      <tr>
-      <td>Juliana </td>
-      <td>Front-end developer</td>
+      <td> </td>
+      <td></td>
       </tr>
       </tbody>
+  )
+}
+
+class Table extends Component {
+  render(){
+    return(
+      <table>
+
+      <TableHeader />
+      <TableBody />
 
       </table>
     )
