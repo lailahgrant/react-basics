57c5b2915f37376e577fab7ff2e3d11a58fe62c2
lailahgrant <lailahgrant19@gmail.com>
2020-05-20 14:31:57 +0300

    added a table file

---
+++ Table.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class Table extends Component {
+  render(){
+    return(
+      <table>
+
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+
+      <tbody>
+      <tr>
+      <td>Lailah </td>
+      <td>Cloud developer</td>
+      </tr>
+      <tr>
+      <td>Kato </td>
+      <td>Doctor</td>
+      </tr>
+      <tr>
+      <td>Waswa </td>
+      <td>Cycber spcialist</td>
+      </tr>
+      <tr>
+      <td>Bushra </td>
+      <td>Intern</td>
+      </tr>
+      <tr>
+      <td>Juliana </td>
+      <td>Front-end developer</td>
+      </tr>
+      </tbody>
+
+      </table>
+    )
+  }
+}
